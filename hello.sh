3906bc607226a2b152a49875dc1d2c9b4f3cbcca
anxielray <anxielworld@gmail.com>
2024-07-14 11:07:47 +0200

    Added author comment

--- hello.sh
+++ hello.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-<<COMMENT
-Line 5 sets a variable name
-to the value of the first command-line argument $1 if it's provided,
-and defaults to "World" if no argument is provided.
-COMMENT
-name=${1:-World}
+
+# Default is World
+# Author: Raymond Ogwel <anxielworld@gmail.com>
+name=${1:-"World"}
+
 echo "Hello, $name"
