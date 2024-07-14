4bc7d5c8e24a041c1d4ad09c31f51ea0f1f96dfc
anxielray <anxielworld@gmail.com>
2024-07-14 06:34:46 +0200

    Added multi-line comment to script

--- hello.sh
+++ hello.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-#Default is "World"
+<<COMMENT
+Line 5 sets a variable name
+to the value of the first command-line argument $1 if it's provided,
+and defaults to "World" if no argument is provided.
+COMMENT
 name=${1:-World}
 echo "Hello, $name"
