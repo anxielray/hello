d5db6bf0e1d1976af1811063b2f5f7cf41f3e520
anxielray <anxielworld@gmail.com>
2024-07-14 11:07:47 +0200

    Added author comment

--- hello.sh
+++ hello.sh
@@ -2,7 +2,8 @@
 
 
 # Default is World
-# Author: Raymond Ogwel <anxielworld@gmail.com>
+# Author: Raymond Ogwel <anxielray@github.com>
+# Email: anxielworld@gmail.com
 name=${1:-"World"}
 
 echo "Hello, $name"
